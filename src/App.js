db1b1bbd5d2a6246741a2ee72d775d5cd8413dfc
Samet <samet.demiralay95@gmail.com>
2020-04-07 12:58:22 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+ const [state, setState] = useState("");
+ useEffect(() => {
+  fetchAdvice();
+ }, []);
 
-export default App;
+ const fetchAdvice = () => {
+  axios
+   .get("https://api.adviceslip.com/advice")
+   .then(response => {
+    const { advice } = response.data.slip;
+    setState(advice);
+   })
+   .catch(error => {
+    console.log(error);
+    setState("NOT DEFINED...")
+   });
+ };
+
+ return (
+  <div className="app">
+   <div className="card">
+    <h1 className="heading"> {state} </h1>
+    <button className="button" onClick={()=>fetchAdvice()} >
+      <span>GIVE ME ADVICE!</span>
+    </button>
+   </div>
+  </div>
+ );
+};
+
+
+
+
+// -> Bu kullanım component her render olduğunda çalışacaktır:
+// useEffect(() => {
+//   ...
+// });
+
+// -> sadece yükleme işlemi sonrası çalışması için:
+// useEffect(() => {
+//   ...
+// },
+// []
+// );
+
+// -> Sadece seçtiğimiz bir değişkenin değeri değiştiğinde çalışması için:
+// useEffect(() => {
+//   ...
+// },
+// [deger]
+// );
